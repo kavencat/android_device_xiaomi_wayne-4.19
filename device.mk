bfb62dff4f7bf91bba3208f11d0f23a09829f709
kavencat <99724336+kavencat@users.noreply.github.com>
2022-09-27 15:34:52 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,6 +16,9 @@ TARGET_USES_UM_4_19 := true
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-arrow
+    
+# PRODUCT_SHIPPING_API_LEVEL indicates the first api level, device has been commercially launched on.
+PRODUCT_SHIPPING_API_LEVEL := 27
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
