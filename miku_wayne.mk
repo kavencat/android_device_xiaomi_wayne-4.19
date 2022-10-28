3b79d31abeb82d95bb9cb049ae5db6fde431302f
kavencat <99724336+kavencat@users.noreply.github.com>
2022-10-28 20:04:31 +0800

    Update miku_wayne.mk

--- miku_wayne.mk
+++ miku_wayne.mk
@@ -4,19 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Maintaier
-MIKU_MASTER := xiaoleGun
-
-# Miku UI OFFICIAL
-TARGET_MIKU_BUILD_VARIANT := OFFICIAL
+# Inherit ShapeShiftOS Official build stuff.
+ARROW_GAPPS := true
+ARROW_OFFICIAL := false
+DEVICE_MAINTAINER := kavencat
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Miku UI stuff.
-$(call inherit-product, vendor/miku/build/product/miku_product.mk)
+# Inherit ShapeShiftOS product configuration
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from wayne device
 $(call inherit-product, device/xiaomi/wayne/device.mk)
@@ -25,7 +24,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := wayne
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := MI 6X
-PRODUCT_NAME := miku_wayne
+PRODUCT_NAME := arrow_wayne
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
