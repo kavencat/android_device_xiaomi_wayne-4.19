648e64577c11359af6c0946d4d547e62348d74b2
kavencat <99724336+kavencat@users.noreply.github.com>
2022-11-16 19:10:13 +0800

    Update and rename miku_wayne.mk to aosp_wayne.mk

--- aosp_wayne.mk
+++ aosp_wayne.mk
@@ -5,10 +5,10 @@
 #
 
 # Maintaier
-MIKU_MASTER := xiaoleGun
+
 
 # Miku UI OFFICIAL
-TARGET_MIKU_BUILD_VARIANT := OFFICIAL
+
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common Miku UI stuff.
-$(call inherit-product, vendor/miku/build/product/miku_product.mk)
+$(call inherit-product, vendor/aosp/build/product/miku_product.mk)
 
 # Inherit from wayne device
 $(call inherit-product, device/xiaomi/wayne/device.mk)
@@ -25,7 +25,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := wayne
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := MI 6X
-PRODUCT_NAME := miku_wayne
+PRODUCT_NAME := aosp_wayne
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
