eafee58839d5ff3e074ecc0060e45bdc74b21e58
kavencat <shi2425803@163.com>
2023-12-26 13:34:33 +0800

    wayne:update GPS to LA.UM.9.11.r1-05100-NICOBAR.0
    Change-Id: I9a691be797ddb1ccad0f67fec81eb512b5032d4b
    Signed-off-by: kavencat <shi2425803@163.com>

--- gps/android/utils/battery_listener.cpp
+++ gps/android/utils/battery_listener.cpp
@@ -191,7 +191,7 @@ BatteryListenerImpl::~BatteryListenerImpl()
                 LOC_LOGe("Transaction error in unregister to HealthHAL death: %s",
                         r.description().c_str());
             }
-		}
+        }
     }
     mDone = true;
     mThread->join();
