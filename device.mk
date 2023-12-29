67b7dae0d0c0c8fc3e514d8345e1e48cc15c64d8
kavencat <shi2425803@163.com>
2023-12-29 12:41:58 +0800

    wayne:build vendor.goodix.hardware.biometrics.fingerprint@2.1
    Change-Id: Iec7fba767d4cee364a643b1127378fcacf98b52b
    Signed-off-by: kavencat <shi2425803@163.com>

--- device.mk
+++ device.mk
@@ -159,7 +159,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    vendor.goodix.hardware.biometrics.fingerprint@2.1
 
 # Fwk detect
 PRODUCT_PACKAGES += \
